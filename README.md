cd2d8614f89bbecbc0c24fbbe91ab475301c9eaa
Nishank <nishankpr2002@gmail.com>
2022-02-19 05:16:59 +0530

    flags added

--- README.md
+++ README.md
@@ -2,3 +2,6 @@
 - Developed DNS server, DNS client and Service Provider(SP) for Private Network.
 - Configured and modified PowerDNS Authoritative & Recursive open-source software with PostgreSQL backend to query(using client), store and allow dynamic updates(using SP) to domain information.
 - Used Google Protocol Buffers(protobuf) for data serialization and BSD socket APIs for communication.
+
+## References:
+- https://datatracker.ietf.org/doc/html/rfc1035
--- dns.py
+++ dns.py
@@ -1,3 +1,4 @@
+# TO create a UDP or TCP connection
 import socket
 
 port = 53 # as DNS operates on port 53
@@ -6,6 +7,18 @@ ip = '127.0.0.1'
 sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
 sock.bind((ip, port))
 
+def buildresponse(data):
+    
+    # Transaction ID
+    TransactionID = data[:2]
+    TID = ''
+    for byte in TransactionID:
+        TID += hex(byte)[2:]
+    
+    # Get the Flags
+    Flags = getflags(data[2:4])
+
 while 1:
     data, addr = sock.recvfrom(512)
-    print(data)
+    r = buildresponse(data)
+    sock.sendto(r, addr)
