e024daf87a942113b943e8a5d77aca6a0c636728
theMillenniumFalcon <nishankpr2002@gmail.com>
2022-04-08 01:33:18 +0530

    algo updated

--- dns.py
+++ dns.py
@@ -29,7 +29,38 @@ def getFlags(flags):
     Z = '000'
     RCODE = '000'
     
-    return int(QR + OPCODE + AA + TC + RD, 2).to_bytes(1, byteorder = 'big') + int(RA + Z + RCODE, 2).to_bytes(1, byteorder= ' big')
+    return int(QR + OPCODE + AA + TC + RD, 2).to_bytes(1, byteorder = 'big') + int(RA + Z + RCODE, 2).to_bytes(1, byteorder= 'big')
+
+def getQuestionDomain(data):
+    state = 0
+    expectedLength = 0
+    domainString = ''
+    domainParts = []
+    x = 0
+    y = 0
+    
+    for byte in data:
+        if state == 1:
+            domainString += chr(byte)
+            x += 1
+            if x == expectedLength:
+                domainParts.append(domainString)
+                domainString = ''
+                state = 0
+                x = 0
+            if byte == 0:
+                domainParts.append(domainString)
+                break
+        else:
+            state = 1
+            expectedLength = byte
+        x += 1
+        y += 1
+        
+    questionType = data[y+1:y+3]
+    print(questionType)
+        
+    return (domainParts, questionType)
 
 def buildresponse(data):
     
@@ -43,6 +74,10 @@ def buildresponse(data):
     Flags = getFlags(data[2:4])
     
     # Question Count
+    QDCOUNT = b'\x00\x01'
+    
+    # Answer Count
+    getQuestionDomain(data[12:])
     
     print(Flags)
 
